9a67837e7a5f02ea8f9e587e14be9d7debeca35e
Radina Staneva <radistaneva@gmail.com>
2012-05-02 16:22:38 +0300

    Catch all exceptions in one place

--- src/FlickrImageSearch.java
+++ src/FlickrImageSearch.java
@@ -24,8 +24,8 @@ public class FlickrImageSearch {
 
 	public static void main(String[] args) {
 
-	    String server = "www.flickr.com";
-	    REST rest;
+		String server = "www.flickr.com";
+		REST rest;
 		try {
 			rest = new REST();
 			rest.setHost(server);
@@ -42,49 +42,38 @@ public class FlickrImageSearch {
 
 			PhotosInterface photosInterface = flickr.getPhotosInterface();
 			PhotoList photoList;
-			try {
-				// change the number of results per page
-				final int perPage = 20;
+			// change the number of results per page
+			final int perPage = 20;
 
-				// this is just to find out the total results and pages
-				photoList = photosInterface.search(searchParams, perPage, 0);
-				int totalPages = photoList.getPages();
-				int totalResults = photoList.getTotal();
-				photoList.clear();
-				
-				File f = new File("images");
-				f.mkdirs();
-				
-				for (int x = 0; x < totalPages; x++) {
-					if (photoList != null){
-						// retrieve each page
-						photoList = photosInterface.search(searchParams, perPage, x);
-						if (photoList != null) {
-							for (int y = 0; y < photoList.size(); y++) {
-								Photo photo = (Photo)photoList.get(y);
-								System.out.println((x * perPage) + y + "/" + totalResults + " url: " + photo.getUrl());
-								try {
-									// save image to file
-									BufferedImage image = photo.getLargeImage();
-									if (image != null) {
-										File outputfile = new File(f.getPath() + File.separator + photo.getId() + ".png");
-										ImageIO.write(image, "png", outputfile);
-									}
-								} catch (IOException e) {
-									e.printStackTrace();
-								}	          
+			// this is just to find out the total results and pages
+			photoList = photosInterface.search(searchParams, perPage, 0);
+			int totalPages = photoList.getPages();
+			int totalResults = photoList.getTotal();
+			photoList.clear();
+
+			File f = new File("images");
+			f.mkdirs();
+
+			for (int x = 0; x < totalPages; x++) {
+				if (photoList != null){
+					// retrieve each page
+					photoList = photosInterface.search(searchParams, perPage, x);
+					if (photoList != null) {
+						for (int y = 0; y < photoList.size(); y++) {
+							Photo photo = (Photo)photoList.get(y);
+							System.out.println((x * perPage) + y + "/" + totalResults + " url: " + photo.getUrl());
+							// save image to file
+							BufferedImage image = photo.getLargeImage();
+							if (image != null) {
+								File outputfile = new File(f.getPath() + File.separator + photo.getId() + ".png");
+								ImageIO.write(image, "png", outputfile);
 							}
 						}
 					}
 				}
-			} catch (IOException e) {
-				e.printStackTrace();
-			} catch (SAXException e) {
-				e.printStackTrace();
-			} catch (FlickrException e) {
-				e.printStackTrace();
 			}
-		} catch (ParserConfigurationException e) {
+
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		System.out.println("Done");
